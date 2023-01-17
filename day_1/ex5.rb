01a30beff7df529f7fbfb0215c64ea4d31d7c8b4
margdaly <margaret.daly@gmail.com>
2023-01-17 01:46:54 -0700

    Initial commit

---
+++ day_1/ex5.rb
@@ -0,0 +1,18 @@
+my_name = 'Margaret K. Daly'   
+my_age = 34
+my_height = 5.7
+my_weight = 160
+my_eyes = 'Blue'
+my_teeth = 'White'
+my_hair = 'Red'
+
+puts "Let's talk about #{my_name}."
+puts "She's #{my_height} inches tall."
+puts "She's #{my_weight} pounds heavy."
+puts "Actually that's not too heavy."
+puts "She's got #{my_eyes} eyes and #{my_hair} hair."
+puts "Her teeth are usually #{my_teeth} depending on the coffee."
+
+# this line is tricky, try to get it exactly right
+puts "If I add #{my_age}, #{my_height}, and #{my_weight} I get #{my_age + my_height + my_weight}."
+
